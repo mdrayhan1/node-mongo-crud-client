df7847e7042c925d30782f730c87d11289593adb
Md. Rayhan <mdrayhanm252@gmail.com>
2022-11-06 03:55:26 +0600

    added post and get

---
+++ _steps .js
@@ -0,0 +1,189 @@
+/* node mongo client CRUD
+1.
+2.npm i react-router-dom
+3.gsearch-react router dom
+4.reacrouter.com er react rrouter:Home v6.4.1-click
+5.tutorial-
+6.
+
+
+*/
+
+/* node-mongo-crud-client a clint side theke server hoya database data pathano suru hoiuce 65-4 er 1 minite theke
+1.gsearch-fetch post example-developer.mozila.org thekeUsing the Fetch API-MDN Web Docs a duka
+2.Uploading Json Data
+3.server er localhost er jagay-slash / dia j route a post korbo seta likhe full url ta copy
+4.client side er component er modde submithandler function er modde fetch likhe parenthesis er modde single cote dia copykrito url ti paste kore koma dia space dia curly braket dia enter{} 
+client side theke pathanor kaj start
+fetch('http://localhost:5000/users', {
+5. .then(res => res.json())
+    .then(data => console.log(data))
+
+  fetch('http://localhost:5000/users', {
+6.  method: 'POST',
+7.  headers: {
+      'content-type': 'application/json'
+    },
+8.  body: JSON.stringify(user)
+  })
+9. .then(res => res.json())
+    .then(data => {
+        console.log(data) console.log ta ka select kore {} curly dia enter dia multiline kora
+    })       
+10.servser side a gia receive kora
+
+
+server side er 7 no porjonto databasr er data load mane read kora hoicilo seta ekhane clint side a home a dekhanor kaj kora hocce 
+2 vave dat ekhane load kora jay, 1st-useeffect er sathe fetch dia, 2nd loader dia 
+1.home cpmponent er element er nice loader dia anonymus arrow function er pore fetch parenthgisis er modde single cote a backend er maddome get kora backend route soho url ta paste 
+2.go home component
+3.
+4.
+5.
+6.
+7.
+8.
+9.
+10.
+21.
+22.
+23.
+24.
+25.
+26.
+27.
+28.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+11.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
