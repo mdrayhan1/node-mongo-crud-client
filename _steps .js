1b8e12c237092aadd8d86caf2d3691f17147bacd
Md. Rayhan <mdrayhanm252@gmail.com>
2022-11-06 09:10:13 +0600

    update part added

--- _steps .js
+++ _steps .js
@@ -118,36 +118,190 @@ mainly simple read specific kawkw na dekhe sokol k dekha hoice ekhane  o pasapas
 9.delete er kaj start
 29, no line backend a
 37, no backend theke nea
-28.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
-11.
+
+65-7 start
+1.component create-[Update.js]-
+2.app.js a route set
+3.home component a link set
+
+mport React from 'react';
+import { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+    const users = useLoaderData();
+    const [displayUsers, setDisplayUsers] = useState(users);
+
+    const handleDelete = user => {
+        const agree = window.confirm(`Are you sure you want to delete: ${user.name}`);
+
+        if (agree) {
+            // console.log('deleting user with id: ', user._id)
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('User deleted successfully.');
+                        const remainingUsers = displayUsers
+                            .filter(usr => usr._id !== user._id);
+                        setDisplayUsers(remainingUsers);
+                    }
+                });
+        }
+    }
+
+    return (
+        <div>
+            <h2>Users: {displayUsers.length}</h2>
+            <div>
+                {
+                    displayUsers.map(user => <p key={user._id}>
+                        {user.name} {user.email}
+3.[home component a link set o er to ajbe update route a dynamic vave user_id te] <Link to={`/update/${user._id}`}>
+4.no backend a                            <button>
+                                update
+                            </button>
+                        </Link>
+                        <button
+                            onClick={() => handleDelete(user)}
+                        >X</button>
+                    </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Home;
+
+5. no server side theke
+5.go app.js-update/id route a loader dea params soho o paramsid dynamicvavae soho
+loader: ({params}) => fetch(`http://localhost:5000/users/${params.id}`)
 
 
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
+const Update = () => {
+ 7.   const storedUser = useLoaderData();
+7.go update route- useloaderdata diya user ta nea
+    
+    const [user, setUser] = useState(storedUser);
 
+    const handleUpdateUser = event =>{
+        event.preventDefault();
+        // console.log(user);
+        fetch(`http://localhost:5000/users/${storedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.modifiedCount > 0){
+                alert('user updated')
+                console.log(data);
+            }
+            
+        })
+    }
 
+    const handleInputChange = event =>{
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = {...user}
+        newUser[field] = value;
+        setUser(newUser);
+    }
+
+    return (
+        <div>
+            <h2>Please Update: {storedUser.name}</h2>
+            <form onSubmit={handleUpdateUser}>
+                <input onChange={handleInputChange} defaultValue={storedUser.name} type="text" name='name' placeholder='name' required />
+                <br />
+                <input onChange={handleInputChange} type="text" defaultValue={storedUser.address} name='address' placeholder='address' required />
+                <br />
+                <input onChange={handleInputChange} type="email" defaultValue={storedUser.email} name="email" id="" placeholder='email' required />
+                <br />
+                <button type="submit">Update User</button>
+            </form>
+        </div>
+    );
+};
+
+export default Update;
+
+8.adduser component theke full form ta copy kore return er nice div er modde h2 er nice paste
+9.ar return er opor theke state porjonto nea
+10.usestate a e komia import
+11.onsubmithanler name f2 dia chage
+12.onsubmit handler er  console.log rekhe baki delete
+13.blur handler ta blur er jagay replace change
+14.onblur a select kore ctrl d dia repalce onchange
+14.1, add user btn er name update user kora
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Update = () => {
+    const storedUser = useLoaderData();
+    
+18.    const [user, setUser] = useState(storedUser);
+18.usestate a parameter stateuser set
+    const handleUpdateUser = event =>{
+        event.preventDefault();
+        // console.log(user);
+        fetch(`http://localhost:5000/users/${storedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.modifiedCount > 0){
+                alert('user updated')
+                console.log(data);
+            }
+            
+        })
+    }
+
+    const handleInputChange = event =>{
+        const field = event.target.name;
+        const value = event.target.value;
+        const newUser = {...user}
+        newUser[field] = value;
+        setUser(newUser);
+    }
+return (
+        <div>
+            <h2>Please Update: {storedUser.name}</h2>
+            <form onSubmit={handleUpdateUser}>
+15.                <input onChange={handleInputChange} defaultValue={storedUser.name} type="text" name='name' placeholder='name' required />
+15. no a //defaultValue={storedUser.name}                
+                <br />
+16.               <input onChange={handleInputChange} type="text" defaultValue={storedUser.address} name='address' placeholder='address' required />
+16. no a // defaultValue={storedUser.address}               
+                <br />
+17.                <input onChange={handleInputChange} type="email" defaultValue={storedUser.email} name="email" id="" placeholder='email' required />
+17. no a // defaultValue={storedUser.email}                
+                <br />
+                <button type="submit">Update User</button>
+            </form>
+        </div>
+    );
+
+19.check ,update user aclick korle console a ta pabo o field a name, mail rename korle tao pabo
+ 65-7 end
+
+ 
 
 
 
